eda9cd7bd966c959d3ef376fdc068fd160c9210c
Wakamori <Wakamori@wakamori-VAIO.(none)>
2011-05-28 04:16:00 +0900

    update classtwitter for non-UNIX os

--- wakamori/classtwitter/classtwitter.py
+++ wakamori/classtwitter/classtwitter.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/c/cygwin/bin/python
 # -*- encoding:utf-8 -*-
 
-import pexpect
+import ssh
 import os
 import time
 import twitter
@@ -20,84 +20,67 @@ CONSUMER_KEY = conf.get('apis', 'consumer_key')
 CONSUMER_SECRET = conf.get('apis', 'consumer_secret')
 ACCESS_KEY = conf.get('apis', 'access_key')
 ACCESS_SECRET = conf.get('apis', 'access_secret')
-PASS = # your pass (ex. os.getenv('CTPASS'))
-
-class Student:
-	def __init__(self, name, time):
-		self.name = name
-		self.time = time
-		self.tweeted = False
-
-	def setTweeted(self):
-		self.tweeted = True
-
-	def isTweeted(self):
-		return self.tweeted
-
-	def getName(self):
-		return self.name
-
-	def getTime(self):
-		return self.time
-
-class RemoteShell:
-	def __init__(self, user=USER, passwd=PASS, host=HOST):
-		self.user = user
-		self.passwd = passwd
-		self.host = host
-		
-	def run(self, command):
-		child = pexpect.spawn('ssh -l %s %s %s' % (self.user, self.host,
-			command))
-		child.expect('%s@%s\'s password:' % (self.user, self.host))
-		child.sendline(self.passwd)
-		child.expect(pexpect.EOF)
-		return child.before
+PASS = conf.get('secrets', 'pass')
 
 class ClassTwitter:
 	def __init__(self, cmd=CMD, cokey=CONSUMER_KEY, cosecret=CONSUMER_SECRET,
-			ackey=ACCESS_KEY, acsecret=ACCESS_SECRET):
+			ackey=ACCESS_KEY, acsecret=ACCESS_SECRET, date=DATE):
 		self.api = twitter.Api(cokey, cosecret, ackey, acsecret)
-		self.sh = RemoteShell()
+		self.s = ssh.Connection(HOST, USER, '', PASS)
 		self.cmd = cmd
-		self.oldstudents = {}
 		self.newstudents = {}
+		self.date = date
+		f = open('./%s_tweeted.txt' % date, 'a+')
+		olds = {}
+		for line in f:
+			s = line.split(",")
+			olds[s[0]] = s[1]
+		self.oldstudents = olds
+		self.f = f
+		self.tweeted = []
 
-	def check(self, date=DATE):
-		newlist = self.sh.run(self.cmd).strip().split('\r\n')[1:]
+	def check(self):
+		count = 0
+		newlist = self.s.execute(self.cmd)[1:]
 		for line in newlist:
 			s = line.split(' ')
-			if not s[2] in self.oldstudents.keys():
-				#if s[7] == date:
-					self.newstudents[s[2]] = Student(s[2], s[8])
+			key = s[9][:-1]
+			if not key in self.newstudents.keys():
+				if not key in self.oldstudents.keys():
+					count = count + 1
+					self.newstudents[key] = s[8]
+		print 'newcount=', count, 'stackcount=', len(self.newstudents)
 
-	def run(self, interval=60):
+	def run(self, interval=10):
 		while True:
 			print 'check time=' + time.strftime("%H:%M:%S")
 			self.check()
-			while len(self.newstudents) > 0:
-				tmp = self.newstudents.popitem()
-				self.oldstudents[tmp[0]] = tmp[1]
-			tweetlist = []
-			for old in self.oldstudents.values():
-				if not old.isTweeted():
-					tweetlist.append(old)
-			if len(tweetlist) > 0:
-				self.tweet(tweetlist)
+			if len(self.newstudents) > 0:
+				self.tweet()
 			time.sleep(interval)
 
-	def tweet(self, students):
+	def tweet(self):
 		message = '[出席確認済] '
-		for student in students:
+		while len(self.newstudents) > 0:
+			s = self.newstudents.popitem()
+			message = message + s[0] + 'さん '
+			self.tweeted.append(s)
 			if len(message) > 140:
 				break
-			message = message + student.getName() + 'さん '
-			student.setTweeted()
-		print 'send ' + message
+		print 'send ' + message.decode('utf-8')
 		try:
 			self.api.PostUpdate(message.decode('utf-8'))
+			self.write()
 		except:
 			print 'can\'t update'
+
+	def write(self):
+		while len(self.tweeted) > 0:
+			s = self.tweeted.pop()
+			print 'write %s %s' % (s[0], s[1])
+			self.f.write(s[0] + ',' + s[1] + '\n')
+			self.oldstudents[s[0]] = s[1]
+		self.f.flush()
 
 if __name__ == '__main__':
 	ct = ClassTwitter()
