48ec0766f48be169cdca200c2a61acf2a025fbdb
utrhira <utr.hira@gmail.com>
2011-05-30 21:16:12 +0900

    update aspen.js

--- shinpei/aspen/js/aspen.js
+++ shinpei/aspen/js/aspen.js
@@ -25,31 +25,34 @@ function escapeAll(text) {
         return resnum++;
     };
     Aspen.postScript = function(text) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                if (xhr.status == 200) {
-                    var view = document.getElementById("result");
-                    view.innerText = xhr.responseText;
+        var view = document.getElementById("result");
+        if (Aspen.requestAllowed()) {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        view.innerText = xhr.responseText;
+                    }
                 }
-            }
-        };
-        xhr.open("POST", location.href, true); // async
-        xhr.setRequestHeader("Content-Type", "text/plain");
-        xhr.send("kscript=" + escapeAll(text));
-        setTimeout("Aspen.allowRequest()", Aspen.timeout);
+            };
+            xhr.open("POST", location.href, true); // async
+            xhr.setRequestHeader("Content-Type", "text/plain");
+            xhr.send("kscript=" + escapeAll(text));
+            setTimeout("Aspen.allowRequest()", Aspen.timeout);
+        } else {
+            view.innerText = "３秒待ってください。";
+        }
     };
 })();
 
 CodeMirror.defaults.onKeyEvent = function (editor, key) {
-    if (key.type == "keydown" && key.shiftKey && key.keyIdentifier == "Enter") {
-        if (Aspen.requestAllowed()) {
-            var text = editor.getValue();
-            if (text.length > 0) {
-                Aspen.postScript(text);
-                key.preventDefault();
-            }
+    if (key.type == "keydown" && key.shiftKey && key.keyCode == 13) {
+        g_key = key;
+        var text = editor.getValue();
+        if (text.length > 0) {
+            Aspen.postScript(text);
         }
+        key.returnValue = false;
     }
 };
 
