a52c0d5fe25ebf0480788a392fe5f0cf358626dd
shinpei nakata <shinpei.nakata@gmail.com>
2011-06-03 20:51:08 +0900

    aspen storage

--- wakamori/aspen/cgi/aspendb.py
+++ wakamori/aspen/cgi/aspendb.py
@@ -7,24 +7,26 @@ AspenStorage:
 
 """
 
-import sqlite3, uuid
+from sqlite3 import dbapi2 as sqlite
+import uuid
 
 """
 globals
 """
 
-DB_NAME = "aspen.sqlite3"
+db_name = "aspen.sqlite3"
+table_name = "aspen"
 
 """
  class AspenSession
 """
 
 class AspenSession:
-    def __init__(uid, passwd):
+    def __init__(self, uid, passwd):
         self.uid = uid;
         self.passwd = passwd;
 
-    def generateSID():
+    def generateSID(self ):
         self.__sid = uuid.uuid4();
 
 
@@ -34,12 +36,29 @@ class AspenStorage
 
 class AspenStorage:
     def __init__(self):
-        self.dbname = DBNAME
-        self.db = sqlite3.connect(DB_NAME, isolation_level=None)
-        self.connection = self.db.cursor()
-
+        self.dbname = db_name;
+        self.con = sqlite.connect(db_name)
+        self.cur = self.con.cursor()
 
     def generateSession(self):
         return AspenSession();
 
+    def authenticate (self, session):
+        uid = session.uid;
+        self.cur.execute('select passwd from personal where uid="' + uid + '";')
+        passwd = ""
+        for row in self.cur:
+            passwd = row[0]
+        if session.passwd == passwd:
+            session.generateSID();
+            return True;
+        return False;
+
+    
+
         
+if __name__ == '__main__':
+    ast = AspenStorage();
+    ses = AspenSession("d11gd186", "konoha");
+    if ast.authenticate(ses) == True:
+        print "authenticate done!"
