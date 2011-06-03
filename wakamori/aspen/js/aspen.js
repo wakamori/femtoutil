76e6a27e771417a289151a25c82bf8d819aec5be
Takuma Wakamori <wakamori111@gmail.com>
2011-06-03 18:01:50 +0900

    aspen.js modified

--- wakamori/aspen/js/aspen.js
+++ wakamori/aspen/js/aspen.js
@@ -101,27 +101,27 @@ if (!Aspen) Aspen = {};
 		Aspen.denyRequest();
 		Aspen.saveCookie();
 		var nowdate = new Date();
-		var filepath = String.format("%02d%02d", nowdate.getMonth(), nowdate.getDate()) + "/" + $.cookie("name");
+		//var filepath = String.format("%02d%02d", nowdate.getMonth(), nowdate.getDate()) + "/" + $.cookie("name");
 		$.PeriodicalUpdater(
-			filepath,
+			"./cgi/reader.cgi",
 			{
 				method: "GET",
 				minTimeout: 100
 			},
 			function(data) {
-//				$("#result").empty()
-//				var json = JSON.parse(data);
-//				for (var i = 0; i < json.length; i++) {
-//					var key = json[i].key;
-//					var val = json[i].value;
-//					if (val.length > 0) {
-//						$("<span/>").attr("class", key).append(val).appendTo("#result");
-//						$("#result").append("<br />");
-//					}
-//				}
+				$("#result").empty()
+				var json = JSON.parse(data);
+				for (var i = 0; i < json.length; i++) {
+					var key = json[i].key;
+					var val = json[i].value;
+					if (val.length > 0) {
+						$("<span/>").attr("class", key).append(val).appendTo("#result");
+						$("#result").append("<br />");
+					}
+				}
 				$("#result").append(data);
 			}
-		});
+		);
 		$.ajax({
 			type: "POST",
 			url: "./cgi/aspen.cgi",
