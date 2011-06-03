465fe8d1b645f0dbc156ab89dc24b40cc0eed970
shinpei nakata <shinpei.nakata@gmail.com>
2011-06-03 22:04:11 +0900

    aspendb update

--- wakamori/aspen/cgi/aspendb.py
+++ wakamori/aspen/cgi/aspendb.py
@@ -8,7 +8,7 @@ AspenStorage:
 """
 
 from sqlite3 import dbapi2 as sqlite
-import uuid
+import uuid, time
 
 """
 globals
@@ -16,6 +16,9 @@ globals
 
 db_name = "aspen.sqlite3"
 table_name = "aspen"
+personal_tbl_name = "personal"
+session_tbl_name = "sessions"
+
 
 """
  class AspenSession
@@ -23,12 +26,20 @@ table_name = "aspen"
 
 class AspenSession:
     def __init__(self, uid, passwd):
-        self.uid = uid;
-        self.passwd = passwd;
+        self.__uid = uid;
+        self.__passwd = passwd;
 
     def generateSID(self ):
         self.__sid = uuid.uuid4();
 
+    def getSID (self):
+        return self.__sid;
+
+    def getUID(self):
+        return self.__uid;
+
+    def getPasswd(self):
+        return self.__passwd;
 
 """
 class AspenStorage
@@ -44,21 +55,55 @@ class AspenStorage:
         return AspenSession();
 
     def authenticate (self, session):
-        uid = session.uid;
+        uid = session.getUID();
         self.cur.execute('select passwd from personal where uid="' + uid + '";')
         passwd = ""
         for row in self.cur:
             passwd = row[0]
-        if session.passwd == passwd:
+        if session.getPasswd() == passwd:
             session.generateSID();
             return True;
         return False;
 
-    
-
+    def commitSession (self, session):
+        if session.getSID() != "" :
+            query = "insert into " + session_tbl_name + " values(";
+            query = query + '"' + str(session.getSID()) + '", ';
+            query = query + '"' + session.getUID() + '", ';
+            query = query + '"' + str(time.mktime(time.localtime())) + '", ';
+            query = query + '"Untitled");';
+            print query;
+            self.cur.execute(query)
+            self.con.commit()
+    def close(self):
+        self.con.close();
         
+
+    # name SID 
+    #  assume that sid is already exists!
+    #  if its not exist, error.
+    def nameSID(self, session, filename):
+        if session.getSID() != "":
+            query = "update " + session_tbl_name + " set ";
+            query = query + 'filename="' + filename + '" where sid="';
+            query = query + str(session.getSID()) + '";';
+            self.cur.execute(query);
+            self.con.commit();
+
+
+    # sid --> filename
+    def sid2filename(self, sid):
+        return;
+
+
+
 if __name__ == '__main__':
     ast = AspenStorage();
     ses = AspenSession("d11gd186", "konoha");
     if ast.authenticate(ses) == True:
+        ast.commitSession(ses);
+        ast.nameSID(ses, "Hello world");
         print "authenticate done!"
+    else :
+        print "authentication failed"
+    ast.close()
