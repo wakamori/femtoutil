485769d0c73247b375f008febcb69dcb338b17a5
Takuma Wakamori <wakamori111@gmail.com>
2011-06-24 22:32:13 +0900

    ring reverted to specify the loop count

--- wakamori/ruby_actor/ring.rb
+++ wakamori/ruby_actor/ring.rb
@@ -23,32 +23,32 @@ def receive(msg)
 	name = msg[0]
 	actor_num = msg[1]
 	actor_count = msg[2]
-	msg_count = msg[3]
+	loop_count = msg[3]
 	if actor_num == actor_count
+		loop_count -= 1
 		actor_num = 1
+		if loop_count == 0
+			sendToActor(name + actor_num.to_s, 'exitRing', [name, actor_num, actor_count])
+		end
 	else
 		actor_num += 1
 	end
-	if msg_count == 0
-		sendToActor(name + 1.to_s, 'exitRing', [name, 1, actor_count])
-	else
-		sendToActor(name + actor_num.to_s, 'receive', [name, actor_num, actor_count, msg_count - 1])
-	end
+	sendToActor(name + actor_num.to_s, 'receive', [name, actor_num, actor_count, loop_count])
 end
 
-def main(actor_count, msg_count)
+def main(actor_count, loop_count)
 	port = 12345
 	for i in 1..actor_count
 		name = 'actor' + i.to_s
 		spawn(name, port)
 		port += 1
 	end
-	sendToActor('actor1', 'receive', ['actor', 1, actor_count, msg_count])
+	sendToActor('actor1', 'receive', ['actor', 1, actor_count, loop_count])
 	act('kick', port)
 end
 
 if __FILE__ == $0
 	actor_count = ARGV[0] == nil ? 3 : ARGV[0].to_i
-	msg_count = ARGV[1] == nil ? 100 : ARGV[1].to_i
-	main(actor_count, msg_count)
+	loop_count = ARGV[1] == nil ? 100 : ARGV[1].to_i
+	main(actor_count, loop_count)
 end
