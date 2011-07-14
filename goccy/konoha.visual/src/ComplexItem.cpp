947cad1f00a3a55f7e48080022a899f36294a5e7
utrhira <utr.hira@gmail.com>
2011-07-15 00:04:01 +0900

    update konoha.visual

--- goccy/konoha.visual/src/ComplexItem.cpp
+++ goccy/konoha.visual/src/ComplexItem.cpp
@@ -105,6 +105,7 @@ void KComplexItem::setColor(QColor *c)
 	int gp_length = gp_list->size();
 	for (int i = 0; i < gp_length; i++) {
 		QGraphicsPolygonItem *gp = gp_list->at(i);
+		gp->setPen(QPen(*c));
 		gp->setBrush(*c);
 	}
 }
@@ -143,7 +144,8 @@ KMETHOD ComplexItem_new(Ctx *ctx, knh_sfp_t *sfp _RIX)
 {
 	NO_WARNING();
 	knh_Array_t *a = sfp[1].a;
-	KComplexItem *c = new KComplexItem(a);
+	if (knh_Array_size(a) > 2) {
+		KComplexItem *c = new KComplexItem(a);
 	knh_RawPtr_t *p = new_RawPtr(ctx, sfp[2].p, c);
 	RETURN_(p);
 }
