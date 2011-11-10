352255858c7b56b49d661fb7cae7eee6069878be
wakamori <wakamori111@gmail.com>
2011-11-10 14:48:23 +0900

    add ash alpha version

---
+++ wakamori/dscript/ash/init/start.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+echo "start konoha with port 50001";
+konoha -p50001 &
+
+echo "start konoha with port 50002";
+konoha -p50002 &
+
+echo "start konoha with port 50003";
+konoha -p50003 &
+
+echo "start konoha with port 50010";
+konoha -p50010 &
+
+echo "start konoha with port 50011";
+konoha -p50011 &
+
+echo "start konoha with port 50012";
+konoha -p50012 &
+
+echo "start konoha with port 50013";
+konoha -p50013 &
+
+echo "start konoha with port 50014";
+konoha -p50014 &
+
+echo "start konoha with port 50020";
+konoha -p50020 &
+
+echo "start konoha with port 50021";
+konoha -p50021 &
+
+echo "start konoha with port 50022";
+konoha -p50022 &
+
+echo "start konoha with port 50030";
+konoha -p50030 &
+
+echo "start konoha with port 50031";
+konoha -p50031 &
+
+echo "start konoha with port 50032";
+konoha -p50032 &
+
+echo "start konoha with port 50033";
+konoha -p50033 &
+
+echo "start konoha with port 50050";
+konoha -p50050 &
+
+echo "start konoha with port 50051";
+konoha -p50051 &
+
+echo "start konoha with port 50052";
+konoha -p50052 &
---
+++ wakamori/dscript/ash/init/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "kill all konoha processes";
+killall konoha
